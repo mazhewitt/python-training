fec9a61f95b24c6ce39d0c1d803e711406b25d53
palandep <prathmesh.palande@ubs.com>
2019-05-16 17:18:20 +0530

    Add files via upload

---
+++ Day 3/Day3 Home Assignment.py
@@ -0,0 +1,7 @@
+#Q1. Write a python program to print only the first two values from the following List 
+fruits = ["apple","mango","banana","blueberry","orange"]
+
+#Q2. Write a python program to remove the value "apple" and insert value "pineapple" in the above list. Print the list once modified
+
+
+#Q3. Write a python program to create a tuple and insert value 1,2,3,4,5,6. Print the total number of values in the tuple
