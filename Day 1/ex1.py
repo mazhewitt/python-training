c7690d2381282b6ae2ca67ee242fe7c83247e13d
palandep <prathmesh.palande@ubs.com>
2019-05-16 17:15:29 +0530

    Add files via upload

---
+++ Day 1/ex1.py
@@ -0,0 +1,16 @@
+print('Hello, world!')  # Text after hash symbol is ignored
+# Comments starts with # and extend till the end of line
+''' This is a multi-line
+comment. Everything between tripe quotes is a comment.'''
+
+print("This is your first program!")  # Text surrounded by quotes (' or ") is called string
+print("This hash symbol # is ignored because it's in string!")
+print("Sometimes you have to type 2 characters to display one: \\")
+print("She said: \"I'm going to learn some Python today!\"")  # YOu can use quotes within string
+print('She said: "I\'m going learn some Python today!"')
+
+# [Q2] Uncomment line below and try to run program again. What happened? Can you fix it?
+# print('Welcome to CodeRed Python!)
+
+# [Q3] Now uncomment this line and try to run program again. How to fix it?
+# print("Good luck!"
