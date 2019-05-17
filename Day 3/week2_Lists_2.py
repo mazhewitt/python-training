1bdd4dfe32432b3b92980d1be8596e747006db01
palandep <prathmesh.palande@ubs.com>
2019-05-17 11:44:23 +0530

    Add files via upload

---
+++ Day 3/week2_Lists_2.py
@@ -0,0 +1,20 @@
+### Excercise 2 ###
+# 2. Let's plan some shopping. We want to get some fruits, vegies and diary products:
+fruits = ['Banana', 'Apple', 'Lemon', 'Orange']
+vegies = ['Carrot', 'Pumpkin']
+diary = ['Milk', 'Cheese', 'Butter']
+
+# 2.1 Check how many product from each category we want to buy
+
+# 2.2 Create one shopping list called basket with all the products
+
+# 2.3 We forgot about cucumber - can you add it to the list of vegies?
+
+# 2.4 Let's check what is in our basket now:
+print(basket)
+# Is cucumber in the basket? Can you explain why?
+
+# We create one more list called sweets:
+sweets = ['Chocolate', 'Biscuits']
+
+# 2.5 Try to add sweets to the basket using append() and extend() methods. Do you see the difference?
---
+++ Day 3/week2_Strings_2.py
@@ -0,0 +1,14 @@
+### Excercise 2 ###
+# 2 Let's try to do some changes with a string 'LETTER'
+
+word = 'LETTER'
+
+# Try to change the first character to 'B' by assignment
+
+word['fill the index'] = 'B'
+
+# Did it work? If not - how to fix it?
+
+# Now, try to do the same with replace() function.
+
+# And check what is the value of word. Has it been updated? If not - why and how to fix it?
