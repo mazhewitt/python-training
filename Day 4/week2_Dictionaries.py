1cd835492edd746843db233154180e6aab02acde
palandep <prathmesh.palande@ubs.com>
2019-05-17 13:21:55 +0530

    Add files via upload

---
+++ Day 4/week2_Dictionaries.py
@@ -0,0 +1,21 @@
+#### Dictionaries #####
+
+### Excercise 1 ###
+# 1. Create a dictionary with 5 country-currency pairs:
+# France - EUR, UK - GBP, Poland - PLN, Australia - AUD, Italy - EUR
+currencyDict = 
+
+# 1.1 Print all the countries available in currencyDict
+
+# 1.2 Print all the currencies available in currencyDict
+
+# 1.3 Some currencies occur more than once. Can you print only unique currencies?
+
+# 1.4 What currency is used in Australia?
+
+# 1.5 What happens if we want to get currency used in Switzerland?
+currencyDist.get('Switzerland')
+# Try to fix the code below, so if currency for Switzerland is not available, a message 'Currency not available' is printed
+currencyDist.get('Switzerland')
+
+# 1.6 Add one more pair: Switzerland - CHF to currencyDict
---
+++ Day 4/week2_Sets.py
@@ -0,0 +1,21 @@
+#### Sets #####
+
+### Excercise 1 ###
+# Let's go back to our shopping list
+fruits = {'Banana', 'Apple', 'Lemon', 'Orange'}
+vegies = {'Carrot', 'Pumpkin'}
+diary = {'Milk', 'Cheese', 'Butter'}
+sweets = {'Chocolate', 'Biscuits'}
+basket = {'Banana', 'Apple', 'Lemon', 'Orange', 'Carrot', 'Pumpkin', 'Milk', 'Cheese', 'Butter', 'Chocolate', 'Biscuits'}
+# Unfortunately not all the products were available in the first shop. We have already bought:
+bag = {'Banana', 'Apple', 'Lemon', 'Orange', 'Chocolate', 'Biscuits', 'Milk'}
+# 1.1 Check how many products are in the bag
+
+# 1.2 Check which diary products we still need to buy (are not in bag)
+
+# 1.3 Check how many products we still need to buy
+
+# In the second shop we bought 4 products Chocolate, Orange, Cheese and Carrot
+# 1.4 Please update the bag with products from the second shop
+
+# 1.5 How many products are in our bag now? Can you explain that?
