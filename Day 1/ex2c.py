43d4c0de9cf9351c9a257db2723d4009724866d1
palandep <prathmesh.palande@ubs.com>
2019-05-20 11:43:35 +0530

    Delete ex2c.py

--- Day 1/ex2c.py
+++
@@ -1,9 +0,0 @@
-# Modify code from previous exercise so that program could greet any user.
-# Use built-in function input() to get user's name.
-# You can display documentation running help(input) in Python console.
-# Hint: use prompt parameter to ask user for name.
-# input() collects input after you hit ENTER
-
-
-# name =
-# print('Hello', '!')
