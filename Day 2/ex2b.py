8df73e67ca9133eabd33423f85021bc82a426322
palandep <prathmesh.palande@ubs.com>
2019-05-20 11:43:15 +0530

    Add files via upload

---
+++ Day 2/ex2b.py
@@ -0,0 +1,5 @@
+# Uncomment and modify lines 4 and 5. After you run this program, it should greet you with your name,
+# ex: Hello Magda! Use a variable called name. """
+
+# name =
+# print('Hello ', '!')
