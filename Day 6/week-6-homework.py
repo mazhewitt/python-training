f6e8170da7df8c470fea3109bfae2069fa3ac199
mazhewitt <mazda@aduki.co.uk>
2019-11-29 15:15:56 +0100

    Added the week 6 homework

---
+++ Day 6/week-6-homework.py
@@ -0,0 +1,20 @@
+# Write a program to  decrypt files using the ROT13 algorithm
+# more information about ROT 13 is here: https://en.wikipedia.org/wiki/ROT13
+
+
+# use the function below to decrypt the sample.txt
+# here is a function that decrypts characters by rotating them 13 places:
+def decrypt(character):
+    abc = "abcdefghijklmnopqrstuvwxyz"
+    if abc.find(character) != -1:
+        de = abc[(abc.find(character) + 13) % 26]
+    else:
+        de = character
+    return de
+
+# you will need to:
+# open the file using open()
+# read the whole file into a single string variable
+# loop through the characters in the string variable calling the decrypt function for each character
+# print out the result
+
